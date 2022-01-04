ae730cd65e89e019c090be1a42368573266da365
REL1980 <rafaelandeau@hotmail.com>
2022-01-03 21:31:08 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TeleprompterConsole;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var lines = ReadFrom("sampleQuotes.txt");
+        foreach (var line in lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+
+
+    // Método de iterador.
+    static IEnumerable<string> ReadFrom(string file)
+{
+    string? line;
+    using (var reader = File.OpenText(file))
+    {
+        while ((line = reader.ReadLine()) != null)
+        {
+            yield return line;
+        }
+    }
+}
+}
